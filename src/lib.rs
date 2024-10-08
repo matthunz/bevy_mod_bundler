2ab2d0e0a775284b150b2a84f1729322dedf2cc7
Matt Hunzinger <matthunz2@gmail.com>
2024-10-08 14:37:25 -0400

    Create plugin and add licenses

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-use bevy::{ecs::system::EntityCommands, prelude::*};
+use bevy::{ecs::system::EntityCommands, prelude::*, utils::hashbrown::HashMap};
+use serde::de::DeserializeOwned;
+use serde_json::Value;
 use std::sync::Arc;
 
+#[derive(Clone)]
 pub struct Bundler {
     spawn_fn: Arc<dyn Fn(EntityCommands) + Send + Sync>,
 }
@@ -31,5 +34,67 @@ impl Bundler {
 
     pub fn spawn(&self, entity_commands: EntityCommands) {
         (self.spawn_fn)(entity_commands)
+    }
+}
+
+#[derive(Default)]
+pub struct AssetPlugin {
+    registry: ComponentRegistry,
+}
+
+impl AssetPlugin {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn add<C>(mut self, name: &str) -> Self
+    where
+        C: Component + Clone + DeserializeOwned,
+    {
+        self.registry.insert::<C>(name);
+        self
+    }
+}
+
+impl Plugin for AssetPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(self.registry.clone())
+            .init_resource::<BundleRegistry>();
+    }
+}
+
+#[derive(Clone, Default, Resource)]
+struct ComponentRegistry {
+    components: HashMap<
+        String,
+        Arc<dyn Fn(Value) -> Box<dyn Fn(EntityCommands) + Send + Sync> + Send + Sync>,
+    >,
+}
+
+impl ComponentRegistry {
+    pub fn insert<C>(&mut self, name: &str)
+    where
+        C: Component + Clone + DeserializeOwned,
+    {
+        self.components.insert(
+            name.to_string(),
+            Arc::new(|value| {
+                let x = serde_json::from_value::<C>(value).unwrap();
+                Box::new(move |mut commands| {
+                    commands.insert(x.clone());
+                })
+            }),
+        );
+    }
+}
+
+#[derive(Clone, Default, Resource)]
+pub struct BundleRegistry {
+    bundles: HashMap<String, Bundler>,
+}
+
+impl BundleRegistry {
+    pub fn spawn(&self, name: &str, entity_commands: EntityCommands) {
+        self.bundles.get(name).unwrap().spawn(entity_commands);
     }
 }
